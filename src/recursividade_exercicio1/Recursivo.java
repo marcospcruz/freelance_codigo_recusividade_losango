ed2cdbbf15067eef30b61fc0f90e6b6e9b260612
marcospcruz <marcpcruz@gmail.com>
2016-03-30 18:39:13 -0300

    comentando o código

--- src/recursividade_exercicio1/Recursivo.java
+++ src/recursividade_exercicio1/Recursivo.java
@@ -6,43 +6,57 @@ public class Recursivo {
 
 	private int n; // total de repetições passado pelo usuário
 
+	// controle de decrementação de linha. Inversão do triângulo
 	private boolean decrementaLinha;
 
 	public static void main(String args[]) {
 
-		new Recursivo().desenhaLosango(10);
+		int numero = 9;
 
-	}
-
-	private void desenhaLosango(int n) {
-		// TODO Auto-generated method stub
-
-		if (n > 9) {
+		// validação de entrada de valores para limitar valores entre 1 e 9.
+		if (numero < 0 || numero > 9) {
 
 			System.out.println("Tamanho inválido. Informar de 1 a 9!");
 			return;
 
 		}
+		new Recursivo().desenhaLosango(numero);
 
+	}
+
+	/**
+	 * Método recursivo para desenhar o Losango
+	 * 
+	 * @param n
+	 */
+	private void desenhaLosango(int n) {
+		// contador de caracteres que formarão a figura.
 		int contador = 0;
+		// controlador de incremetação e decrementação do contador que formará a
+		// figura.
 		boolean meio = false;
-
+		// configurando o valor passado pelo usuário
 		setN(n);
-
+		// Loop de repetição.
 		while (linha >= 0) {
 
-			int limiteDaLinha = this.n - linha;
-
 			for (int i = 0; i <= this.n + linha; i++) {
+				// condição para marcar os espaços em branco.
+				if (i < (this.n - linha) || i > this.n + linha) {
 
-				if (i < (limiteDaLinha) || i > this.n + linha) {
 					System.out.print(" ");
+					// condição para desenhar a figura.
 				} else {
-
+					// desenhando a figura
 					if (contador > linha || meio == true) {
+						// decrementação do contador, caractere que formará a
+						// figura.
 						contador--;
 						meio = true;
+
 					} else
+						// decrementação do contador, caractere que formará a
+						// figura.
 						contador++;
 
 					System.out.print(contador);
@@ -50,24 +64,34 @@ public class Recursivo {
 				}
 
 			}
+			// pulando para a próxima linha
 			System.out.println();
 
+			// marcação do meio do losango
 			if (linha == this.n - 1)
 				this.decrementaLinha = true;
 
+			// incrementando ou decrementando o losango conforme a marcação do
+			// meio.
 			if (!decrementaLinha)
 				linha++;
 			else
 				linha--;
 
+			// chamada recursiva deste método
 			desenhaLosango(linha);
 
 		}
 
 	}
 
+	/**
+	 * Método responsável em armazenar o valor passado pelo usuário se o aributo
+	 * n for 0(zero). for 0.
+	 * 
+	 * @param n
+	 */
 	private void setN(int n) {
-		// TODO Auto-generated method stub
 
 		if (this.n == 0)
 
